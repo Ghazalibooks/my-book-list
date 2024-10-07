d602971519b28cedd8b900156018c6902ead766b
Yusuf <oezyusuf@web.de>
2024-10-07 16:16:17 +0200

    Added Firebase configuration

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJS7twiAm9Wp5LkOuJKCSQl8xi0SD1t7E",
+  authDomain: "ghazalibooklistapp.firebaseapp.com",
+  projectId: "ghazalibooklistapp",
+  storageBucket: "ghazalibooklistapp.appspot.com",
+  messagingSenderId: "1090094989131",
+  appId: "1:1090094989131:web:32d2f17937d00460b1b2ce",
+  measurementId: "G-8JME6971T3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);  // Authentication
+export const db = getFirestore(app);  // Firestore Database
